92b6ba42e421cb4297d2b3b8dfe45af4c9aded53
ujstha <ujjawalshrestha7@gmail.com>
2024-08-22 21:39:23 +0300

    feat-components: footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,44 +7,46 @@ const Footer: React.FC = () => {
   return (
     <footer className="relative bottom-0 py-12 bg-accent1-10">
       <Container>
-        <div className="flex flex-col justify-evenly gap-6 md:flex-row">
-          <div className="flex-1">
+        <div className="flex flex-col justify-between gap-6 md:flex-row">
+          <div className="max-w-[40%] flex-1">
             <Logo />
-            <p className="mt-3 text-sm">
-              The expected release date of the beta version is during the second
-              quarter of 2025
+            <p className="mt-3 text-balance text-sm lg:text-base">
+              The expected release date of the beta version is during the 2
+              <sup>nd</sup> quarter of 2025
             </p>
             <div></div>
           </div>
-          <div className="flex-1">
-            <h4 className="mb-2 px-3 text-lg font-semibold">Information</h4>
-            <ul>
-              <li>
-                <Link>Terms & Condition</Link>
-              </li>
-              <li>
-                <Link>Privacy & Policy</Link>
-              </li>
-              <li>
-                <Link>Refund Policy</Link>
-              </li>
-              <li>
-                <Link>Community</Link>
-              </li>
-              <li>
-                <Link>Forum</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <h4 className="mb-2 px-3 text-lg font-semibold">Useful Links</h4>
-            <ul>
-              {NAV_ITEMS.map(({ label, href }) => (
-                <li key={label}>
-                  <Link href={href}>{label}</Link>
+          <div className="flex max-w-[60%] flex-1">
+            <div className="flex-1">
+              <h4 className="mb-2 px-3 text-lg font-semibold">Information</h4>
+              <ul>
+                <li>
+                  <Link>Terms & Condition</Link>
                 </li>
-              ))}
-            </ul>
+                <li>
+                  <Link>Privacy & Policy</Link>
+                </li>
+                <li>
+                  <Link>Refund Policy</Link>
+                </li>
+                <li>
+                  <Link>Community</Link>
+                </li>
+                <li>
+                  <Link>Forum</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <h4 className="mb-2 px-3 text-lg font-semibold">Useful Links</h4>
+              <ul>
+                {NAV_ITEMS.map(({ label, href }) => (
+                  <li key={label}>
+                    <Link href={href}>{label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
         <hr className="mt-12 border border-primary-10" />
