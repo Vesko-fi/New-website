5bf8ef4d33b11818e1a6b9c813fda895f372efa6
SantoshChapagai <you@example.com>
2024-08-22 16:23:10 +0300

    create layout and add content for USP options

--- src/components/home/OptionItems.tsx
+++ src/components/home/OptionItems.tsx
@@ -1,9 +1,9 @@
 interface OptionItemsProps {
-    imageSrc: string;
-    heading: string;
-    text: string;
-    height: number;
-    width: number;
+  imageSrc: string;
+  heading: string;
+  text: string;
+  height: number;
+  width: number;
 }
 const OptionItems: React.FC<OptionItemsProps> = ({
   imageSrc,
@@ -13,11 +13,11 @@ const OptionItems: React.FC<OptionItemsProps> = ({
   width,
 }) => {
   return (
-    <div className="flex flex-col items-center lg:w-[358px] p-6 bg-white rounded-lg gap-2 border border-gray-300 border-opacity-20">  
-      <img src={imageSrc} alt={heading} height={height} width={width}/>
-       <h2 className="text-2xl font-bold text-center">{heading}</h2> 
-       <p className="text-base font-normal text-center">{text}</p>
+    <div className="flex flex-col items-center gap-2 rounded-lg border border-gray-300 border-opacity-20 bg-white p-6 lg:w-[358px]">
+      <img src={imageSrc} alt={heading} height={height} width={width} />
+      <h2 className="text-center text-2xl font-bold">{heading}</h2>
+      <p className="text-center text-base font-normal">{text}</p>
     </div>
-  )
-}
-export {OptionItems}
+  );
+};
+export { OptionItems };
--- src/features/home/Options.tsx
+++ src/features/home/Options.tsx
@@ -1,51 +1,58 @@
-import { OptionItems } from '@components/home/OptionItems';
-import { Container } from '@components/ui/Container';
-import { Link } from '@components/ui/Link';
-import { Section } from '@components/ui/Section';
-import { securePaymentImg, optimizeLogisticImg, orderManagementImg } from '@constants/assets';
-import { useTranslation } from 'react-i18next';
+import { OptionItems } from "@components/home/OptionItems";
+import { Container } from "@components/ui/Container";
+import { Link } from "@components/ui/Link";
+import { Section } from "@components/ui/Section";
+import {
+  securePaymentImg,
+  optimizeLogisticImg,
+  orderManagementImg,
+} from "@constants/assets";
+import { useTranslation } from "react-i18next";
 
 const Options: React.FC = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
-    <Section style={{ background: 'linear-gradient(180deg, #FFFFFF 0%, #F0F5FF 100%)' }} className='w-100%'>
-      <Container className='flex flex-col md:flex-row items-center gap-8 lg:gap-16 px-2 md:px-8 lg:px-12'>
+    <Section
+      style={{
+        background: "linear-gradient(180deg, #FFFFFF 0%, #F0F5FF 100%)",
+      }}
+      className="w-100%"
+    >
+      <Container className="flex flex-col items-center gap-8 px-2 md:flex-row md:px-8 lg:gap-16 lg:px-12">
         <div>
-          <h2 className='text-3xl font-bold'>
-            {t('options.heading')}
-          </h2>
+          <h2 className="text-3xl font-bold">{t("options.heading")}</h2>
           <Link variant="primary" size="lg" href="/" className="mt-8">
-          {t('options.link')}
+            {t("options.link")}
           </Link>
         </div>
-        <div className='flex flex-col gap-8 lg:gap-16'>
+        <div className="flex flex-col gap-8 lg:gap-16">
           <OptionItems
             imageSrc={securePaymentImg}
-            heading={t('options.paymentHeading')}
-            text={t('options.paymentText')}
+            heading={t("options.paymentHeading")}
+            text={t("options.paymentText")}
             height={180}
             width={210}
           />
           <OptionItems
             imageSrc={orderManagementImg}
-            heading={t('options.orderHeading')}
-            text={t('options.orderText')}
+            heading={t("options.orderHeading")}
+            text={t("options.orderText")}
             height={180}
             width={236}
           />
         </div>
         <div>
-        <OptionItems
+          <OptionItems
             imageSrc={optimizeLogisticImg}
-            heading={t('options.logisticHeading')}
-            text={t('options.logisticText')}
+            heading={t("options.logisticHeading")}
+            text={t("options.logisticText")}
             height={180}
             width={208}
-          /> 
+          />
         </div>
       </Container>
     </Section>
-  )
-}
+  );
+};
 Options.displayName = "Options";
-export default Options
+export default Options;
