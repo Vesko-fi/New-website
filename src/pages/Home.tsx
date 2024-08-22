95a5e84118daaa538104866612b4145ece1511d4
ujstha <ujjawalshrestha7@gmail.com>
2024-08-22 13:24:39 +0300

    feat-components: Footer; cleaned Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 
 import { SEO } from "@components/SEO";
-import { Button } from "@components/ui/Button";
-import { Container } from "@components/ui/Container";
-import { Section } from "@components/ui/Section";
 import { HeroSection } from "@features/home/HeroSection";
 
 const Home: React.FC = () => {
-  const { t } = useTranslation();
-
   return (
     <>
       <SEO
@@ -18,34 +12,6 @@ const Home: React.FC = () => {
         keywords="vesko, services"
       />
       <HeroSection />
-      <Section>
-        <Container>
-          <h1 className="text-3xl font-bold">{t("welcome")}</h1>
-          <Button>Vesko</Button>
-          <Button variant="outline" size="lg">
-            Accent 1 lg
-          </Button>
-          <Button variant="link">Accent 1</Button>
-          <Button variant="ghost">Accent 1</Button>
-          <Button variant="secondary" size="sm">
-            Accent 1
-          </Button>
-        </Container>
-      </Section>
-      <Section>
-        <Container className="bg-accent1-50">
-          <h1 className="text-3xl font-bold">{t("welcome")}</h1>
-          <Button>Vesko</Button>
-          <Button variant="outline" size="lg">
-            Accent 1 lg
-          </Button>
-          <Button variant="link">Accent 1</Button>
-          <Button variant="ghost">Accent 1</Button>
-          <Button variant="secondary" size="sm">
-            Accent 1
-          </Button>
-        </Container>
-      </Section>
     </>
   );
 };
