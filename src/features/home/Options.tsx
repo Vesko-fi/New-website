44167dbce864dbe1ff6ae8c1c5bb953232b171bb
SantoshChapagai <you@example.com>
2024-08-23 14:10:52 +0300

    changes the style of the options card

--- src/features/home/Options.tsx
+++ src/features/home/Options.tsx
@@ -5,27 +5,27 @@ import { Container } from "@components/ui/Container";
 import { Link } from "@components/ui/Link";
 import { Section } from "@components/ui/Section";
 import {
-  securePaymentImg,
   optimizeLogisticImg,
   orderManagementImg,
+  securePaymentImg,
 } from "@constants/assets";
 
 const Options: React.FC = () => {
   const { t } = useTranslation();
-
+  
   return (
-    <Section className="bg-gradient-to-b from-white to-[#F0F5FF]">
-      <Container className="flex flex-col items-center gap-4 px-2 lg:flex-row lg:gap-12">
+    <Section className="bg-gradient-to-t from-[#F0F5FF]">
+      <Container className="flex flex-col gap-4 px-2 lg:gap-12 xl:flex-row xl:items-center">
         <div>
-          <h2 className="max-w-4xl text-3xl font-bold">
+          <h2 className="max-w-2xl text-2xl font-bold lg:text-3xl">
             {t("options.heading")}
           </h2>
           <Link variant="primary" size="lg" href="/" className="mt-8">
             {t("options.link")}
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-4 md:flex-row">
-          <div className="flex flex-col gap-4 md:flex-row lg:flex-col">
+        <div className="flex flex-1 flex-col items-center justify-center gap-4 md:flex-row">
+          <div className="flex w-full flex-1 flex-col gap-4 lg:items-end">
             <OptionItems
               imageSrc={securePaymentImg}
               heading={t("options.paymentHeading")}
@@ -38,16 +38,16 @@ const Options: React.FC = () => {
               heading={t("options.orderHeading")}
               text={t("options.orderText")}
               height={180}
-              width={236}
+              width={210}
             />
           </div>
-          <div>
+          <div className="w-full flex-1">
             <OptionItems
               imageSrc={optimizeLogisticImg}
               heading={t("options.logisticHeading")}
               text={t("options.logisticText")}
               height={180}
-              width={208}
+              width={210}
             />
           </div>
         </div>
