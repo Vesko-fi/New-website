a7217e4db8a065b19c543c242498b4a9826575be
SantoshChapagai <you@example.com>
2024-08-30 15:58:47 +0300

    fix the styling of the mission section

--- src/features/users/Mission.tsx
+++ src/features/users/Mission.tsx
@@ -13,8 +13,8 @@ const Mission = () => {
       style={{ backgroundImage: `url(${ReinDeer})` }}
     >
       <Container className="flex flex-col items-center justify-between gap-12">
-        <div className="flex flex-col gap-8 px-40 text-center">
-          <h1 className="mb-4 text-2xl font-bold lg:text-3xl xl:text-4xl">
+        <div className="flex flex-col gap-2 lg:gap-8 px-6 md: md:px-16 lg:px-40 text-center">
+          <h1 className="mb-2 md:mb-4 text-2xl font-bold lg:text-3xl xl:text-4xl">
             {t("users.missionHeading")}
           </h1>
           <p>{t("users.missionP1")}</p>
