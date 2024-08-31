a9441b3be4038e1026936f20d23b401bf389d3d3
SantoshChapagai <you@example.com>
2024-08-31 18:17:46 +0300

    implement translation to the option section of users"

--- src/features/users/BuyingOptions.tsx
+++ src/features/users/BuyingOptions.tsx
@@ -3,31 +3,6 @@ import { useTranslation } from "react-i18next";
 import { Container } from "@components/ui/Container";
 import { Section } from "@components/ui/Section";
 import { Card } from "@components/ui/Card";
-import { Icon } from "@components/ui/Icon";
-
-
-const BuyingOptionItems = [
-  {
-    imageSrc: <Icon icon="box"/>,
-    heading: "Product Availability",
-    text: "Why travel long distances to find a product? Through Vesko you can check who sells it and if it's available in-store or not.",
-  },
-  {
-    imageSrc: <Icon icon="hand-coin"/>,
-    heading: "BOPIS",
-    text: "Vesko helps you to find products from local retails, Buy them Online in Vesko and Pick them In Store.",
-  },
-  {
-    imageSrc: <Icon icon="product-booking"/>,
-    heading: "Product Booking",
-    text: "Want to buy the last item but unsure if it meets your needs? Vesko lets you easily book it online.",
-  },
-  {
-    imageSrc: <Icon icon="truck-delivery"/>,
-    heading: "Home Delivery",
-    text: "Prefer to stay home?We’ll deliver from your local store straight to your door.",
-  },
-];
 
 const BuyingOptions = () => {
   const { t } = useTranslation();
@@ -37,17 +12,30 @@ const BuyingOptions = () => {
       <Container>
         <div>
           <h1 className="mb-4 text-center text-2xl font-bold lg:text-3xl xl:text-4xl">
-            {t("users.heroHeading")}
+            {t("users.optionMainHeading")}
           </h1>
         </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-          {BuyingOptionItems.map((item) => (
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
             <Card
-              icon={item.imageSrc}
-              title={item.heading}
-              description={item.text}
+              icon= "box"
+              title= {t("users.option1Heading")}
+              description= {t("users.option1Text")}
             />
-          ))}
+            <Card
+              icon= "hand-coin"
+              title= {t("users.option2Heading")}
+              description= {t("users.option2Text")}
+            />
+            <Card
+              icon= "product-booking"
+              title= {t("users.option3Heading")}
+              description= {t("users.option3Text")}
+            />
+            <Card
+              icon= "truck-delivery"
+              title= {t("users.option4Heading")}
+              description= {t("users.option4Text")}
+            />
         </div>
       </Container>
     </Section>
