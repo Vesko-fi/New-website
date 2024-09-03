a9313d20942a7591b0cc29c860d85cecdcb76294
SantoshChapagai <you@example.com>
2024-09-03 11:07:28 +0300

    add gap between header and item in the buying options

--- src/features/users/BuyingOptions.tsx
+++ src/features/users/BuyingOptions.tsx
@@ -11,31 +11,31 @@ const BuyingOptions = () => {
     <Section className="bg-gradient-to-t from-[#F0F5FF]">
       <Container>
         <div>
-          <h1 className=" mb-6 lg:mb-12 text-center text-2xl font-bold lg:text-3xl xl:text-4xl">
+          <h1 className="mb-6 text-center text-2xl font-bold lg:mb-12 lg:text-3xl xl:text-4xl">
             {t("users.optionMainHeading")}
           </h1>
         </div>
         <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-            <Card
-              icon= "box"
-              title= {t("users.option1Heading")}
-              description= {t("users.option1Text")}
-            />
-            <Card
-              icon= "hand-coin"
-              title= {t("users.option2Heading")}
-              description= {t("users.option2Text")}
-            />
-            <Card
-              icon= "product-booking"
-              title= {t("users.option3Heading")}
-              description= {t("users.option3Text")}
-            />
-            <Card
-              icon= "truck-delivery"
-              title= {t("users.option4Heading")}
-              description= {t("users.option4Text")}
-            />
+          <Card
+            icon="box"
+            title={t("users.option1Heading")}
+            description={t("users.option1Text")}
+          />
+          <Card
+            icon="hand-coin"
+            title={t("users.option2Heading")}
+            description={t("users.option2Text")}
+          />
+          <Card
+            icon="product-booking"
+            title={t("users.option3Heading")}
+            description={t("users.option3Text")}
+          />
+          <Card
+            icon="truck-delivery"
+            title={t("users.option4Heading")}
+            description={t("users.option4Text")}
+          />
         </div>
       </Container>
     </Section>
