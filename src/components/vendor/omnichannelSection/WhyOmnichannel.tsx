a2e94967fb95057100100666a3e23dd4c1dfaaa8
sabin <sashre@utu.fi>
2024-09-05 21:27:33 +0300

    text alignment fixed

--- src/components/vendor/omnichannelSection/WhyOmnichannel.tsx
+++ src/components/vendor/omnichannelSection/WhyOmnichannel.tsx
@@ -8,14 +8,14 @@ const WhyOmnichannel: React.FC = () => {
     <>
       <div className="flex flex-col items-center justify-between gap-12 py-16 md:flex-row">
         <div>
-          <h1 className="mb-4 text-balance text-center text-2xl font-bold lg:text-3xl xl:text-4xl">
+          <h1 className="mb-4 text-balance text-center text-2xl font-bold lg:text-start lg:text-3xl xl:text-4xl">
             {t("vendors.omnichannelHeading")}
             <span className="text-accent1">
               {t("vendors.omnichannelHeadingSpan")}
             </span>
             ?
           </h1>
-          <p className="text-balance text-center text-sm lg:text-base">
+          <p className="text-balance text-center text-sm lg:text-start lg:text-base">
             {t("vendors.omnichannelText")}
           </p>
         </div>
