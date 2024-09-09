1c9e28f4b3ddc1c1948c7fd55112506755c6013e
SantoshChapagai <you@example.com>
2024-09-09 15:55:08 +0300

    implement translation

--- src/features/about/HeroSection.tsx
+++ src/features/about/HeroSection.tsx
@@ -1,33 +1,26 @@
+import { useTranslation } from "react-i18next";
+
 import { Container } from "@components/ui/Container";
 import { Section } from "@components/ui/Section";
 
 const HeroSection: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <Section>
       <Container className="flex flex-col items-center justify-between gap-12 md:flex-row">
         <div>
           <h1 className="mb-4 text-center text-2xl font-bold lg:text-3xl xl:text-4xl">
-            Who we are
+            {t('about.heroHeading')}
           </h1>
           <p className="text-sm xl:text-base">
-            Vesko is a next-generation marketplace offering omnichannel
-            solutions for both vendors and customers. It provides a unique
-            opportunity for physical-only businesses to establish an online
-            presence and expand their reach, while also helping businesses with
-            existing online stores connect with more customers.
+            {t("about.heroP1")}
           </p>
           <p className="text-sm xl:text-base">
-            For buyers, Vesko offers an extensive range of products, including
-            items from small local shops that were previously unavailable
-            online. Customers can easily check product availability, saving them
-            unnecessary trips if an item is out of stock. They also have the
-            option to reserve items for in-store pickup or purchase online for
-            doorstep delivery.
+          {t("about.heroP2")}
           </p>
           <p className="text-sm xl:text-base">
-            Vesko is dedicated to helping businesses grow by connecting them
-            with a larger customer base, while offering shoppers a wide
-            selection of products to choose from.
+          {t("about.heroP3")}
           </p>
         </div>
       </Container>
