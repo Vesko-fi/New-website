d3c6f66c0b42e2abb9ac92e2d67d56a427c1f3dc
sabin <sashre@utu.fi>
2024-09-09 17:33:49 +0300

    faq heading removed

--- src/features/vendor/Faqs.tsx
+++ src/features/vendor/Faqs.tsx
@@ -10,33 +10,32 @@ const Faqs: React.FC = () => {
     <Section className="bg-gradient-to-t from-[#F0F5FF]">
       <Container className="py-16">
         <div className="mb-8 flex flex-col items-center gap-4">
-          <h4 className="text-accent1">FAQ</h4>
           <h1 className="mb-1 text-center text-2xl font-bold sm:text-balance lg:text-3xl xl:text-4xl">
-            {t("vendors.faqHeading")}
+            {t("vendors.pointOfSale.faqHeading")}
           </h1>
           <p className="text-balance text-center text-base">
-            {t("vendors.faqText")}
+            {t("vendors.pointOfSale.faqText")}
           </p>
         </div>
         <Accordion
-          title={t("vendors.faqQuestion1")}
-          children={t("vendors.faqAnswer1")}
+          title={t("vendors.pointOfSale.faqQuestion1")}
+          children={t("vendors.pointOfSale.faqAnswer1")}
         />
         <Accordion
-          title={t("vendors.faqQuestion2")}
-          children={t("vendors.faqAnswer2")}
+          title={t("vendors.pointOfSale.faqQuestion2")}
+          children={t("vendors.pointOfSale.faqAnswer2")}
         />
         <Accordion
-          title={t("vendors.faqQuestion3")}
-          children={t("vendors.faqAnswer3")}
+          title={t("vendors.pointOfSale.faqQuestion3")}
+          children={t("vendors.pointOfSale.faqAnswer3")}
         />
         <Accordion
-          title={t("vendors.faqQuestion4")}
-          children={t("vendors.faqAnswer4")}
+          title={t("vendors.pointOfSale.faqQuestion4")}
+          children={t("vendors.pointOfSale.faqAnswer4")}
         />
         <Accordion
-          title={t("vendors.faqQuestion5")}
-          children={t("vendors.faqAnswer5")}
+          title={t("vendors.pointOfSale.faqQuestion5")}
+          children={t("vendors.pointOfSale.faqAnswer5")}
         />
       </Container>
     </Section>
