9b4feec55ef839b0df4f506b1500959c87debf5d
SantoshChapagai <you@example.com>
2024-09-10 10:29:10 +0300

    add test image for about us page

--- src/features/about/HeroSection.tsx
+++ src/features/about/HeroSection.tsx
@@ -2,14 +2,15 @@ import { useTranslation } from "react-i18next";
 
 import { Container } from "@components/ui/Container";
 import { Section } from "@components/ui/Section";
+import { heroImg } from "@constants/assets";
 
 const HeroSection: React.FC = () => {
   const { t } = useTranslation();
 
   return (
     <Section>
-      <Container className="flex flex-col items-center justify-between gap-12 md:flex-row">
-        <div>
+      <Container className="py-6">
+        <div className="flex flex-col gap-4">
           <h1 className="mb-4 text-center text-2xl font-bold lg:text-3xl xl:text-4xl">
             {t("about.heroHeading")}
           </h1>
@@ -17,6 +18,7 @@ const HeroSection: React.FC = () => {
           <p className="text-sm xl:text-base">{t("about.heroP2")}</p>
           <p className="text-sm xl:text-base">{t("about.heroP3")}</p>
         </div>
+        <img src={heroImg} alt="" className="mx-auto mt-8 h-96 sm:w-auto" />
       </Container>
     </Section>
   );
