1e850804c0549c2a6865c5445bd7d54f7a21b8d4
SantoshChapagai <you@example.com>
2024-09-11 18:10:27 +0300

    use background image on the hero section of user page

--- src/features/users/HeroSection.tsx
+++ src/features/users/HeroSection.tsx
@@ -8,15 +8,15 @@ const HeroSection: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <Section className="bg-gradient-to-t from-[#E1F4EF]">
-      <Container className="py-16">
-        <div>
-          <h1 className="mb-1 text-center text-2xl font-bold sm:text-balance lg:text-3xl xl:text-4xl">
+    <Section
+      className="bg-cover bg-center flex items-center h-[92vh]"
+      style={{ backgroundImage: `url(${UserHeroImg})` }}
+    >
+      <Container className="flex flex-col justify-start gap-8 text-white">
+          <h1 className="mb-1 text-6xl text-center md:text-left font-bold sm:text-balance md:text-8xl shadow-lg">
             {t("users.hero.heading")}
           </h1>
-          <p className="text-center text-base">{t("users.hero.text")}</p>
-        </div>
-        <img src={UserHeroImg} alt="" className="mx-auto mt-8 h-96 sm:w-auto" />
+          <p className="text-3xl text-center md:text-left md:text-4xl font-bold shadow-lg">{t("users.hero.text")}</p>
       </Container>
     </Section>
   );
