cbd0c30cae5737f2ffa898b4957560fae96802d5
SantoshChapagai <you@example.com>
2024-09-12 09:55:55 +0300

    fix the hero section of user

--- src/features/users/HeroSection.tsx
+++ src/features/users/HeroSection.tsx
@@ -9,22 +9,21 @@ const HeroSection: React.FC = () => {
   return (
     <Section>
       <div className="relative h-[600px] overflow-hidden md:h-[682px]">
-      <div className="h-full w-full bg-cover bg-center">
+        <div className="h-full w-full bg-cover bg-center">
           <img
             src={UserHeroImg}
             alt={`shopping`}
             className="h-full w-full object-cover"
           />
         </div>
-        <div className="absolute inset-0 bg-black/20 flex flex-col justify-center px-8 py-12 text-white sm:px-8">
-        <h1 className="max-w-4xl text-3xl font-bold sm:text-balance md:text-5xl lg:text-6xl xl:text-7xl">
-          {t("users.hero.heading")}
-        </h1>
-        <p className="xl:4xl mt-2 text-xl sm:mt-4 md:text-2xl lg:mt-6 lg:text-3xl">
-          {t("users.hero.text")}
-        </p>
-       </div>
-
+        <div className="absolute inset-0 flex flex-col justify-center bg-black/20 px-8 py-12 text-white sm:px-8">
+          <h1 className="max-w-4xl text-3xl font-bold sm:text-balance md:text-5xl lg:text-6xl xl:text-7xl">
+            {t("users.hero.heading")}
+          </h1>
+          <p className="xl:4xl mt-2 text-xl sm:mt-4 md:text-2xl lg:mt-6 lg:text-3xl">
+            {t("users.hero.text")}
+          </p>
+        </div>
       </div>
     </Section>
   );
