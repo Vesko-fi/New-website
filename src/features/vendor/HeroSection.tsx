e5a58bd1f06e9c8fd5c2b2eb8c701d13fa4adae8
sabin <sashre@utu.fi>
2024-09-12 09:45:20 +0300

    fixed padding

--- src/features/vendor/HeroSection.tsx
+++ src/features/vendor/HeroSection.tsx
@@ -18,11 +18,11 @@ const HeroSetion: React.FC = () => {
         </div>
 
         {/* Overlay text */}
-        <div className="absolute inset-0 bg-black/50 px-8 py-12 text-white sm:px-8">
+        <div className="absolute inset-0 bg-black/50 px-8 py-16 text-white sm:px-14">
           <h1 className="max-w-4xl text-3xl font-bold sm:text-balance md:text-5xl lg:text-6xl xl:text-7xl">
             {t("vendors.hero.heading")}
           </h1>
-          <p className="xl:4xl mt-2 text-xl sm:mt-4 md:text-2xl lg:mt-6 lg:text-3xl">
+          <p className="xl:4xl mt-2 max-w-2xl text-xl sm:mt-4 md:text-2xl lg:mt-6 lg:text-3xl">
             {t("vendors.hero.text")}
           </p>
         </div>
