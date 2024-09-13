5eea4c99ad07e75ca81f9037b3f42f1af98b9d01
sabin <sashre@utu.fi>
2024-09-13 15:42:48 +0300

    footer link updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { NAV_ITEMS, SOCIALS } from "@constants/lists";
 import { Container } from "./ui/Container";
 import { Icon } from "./ui/Icon";
 import { Logo } from "./ui/Logo";
-import { Link } from "./ui/Link";
+import { NavLink } from "react-router-dom";
 
 const Footer: React.FC = () => {
   const { t } = useTranslation();
@@ -45,8 +45,18 @@ const Footer: React.FC = () => {
               <h4 className="mb-2 px-3 text-lg font-semibold">{}</h4>
               <ul>
                 {NAV_ITEMS.map(({ label, href }) => (
-                  <li key={label}>
-                    <Link href={`/${t(href)}`}> {t(label)}</Link>
+                  <li
+                    key={t(label)}
+                    className="cursor-pointer rounded-md px-3 py-2 text-sm font-medium transition-all duration-200 hover:text-accent1 hover:bg-accent1-10"
+                  >
+                    <NavLink
+                      to={`/${t(href)}`}
+                      className={({ isActive }) =>
+                        `${isActive ? "text-accent1 underline underline-offset-4" : "underline-offset-0"}`
+                      }
+                    >
+                      {t(label)}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
