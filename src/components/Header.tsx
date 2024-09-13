07cf60cff7f59106c551346b6ea8d370755ca0f1
sabin <sashre@utu.fi>
2024-09-14 00:13:07 +0300

    hamburger menu in safari works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-
 import { NavItem } from "./nav/NavItem";
 import { ToggleLocale } from "./ToggleLocale";
 import { Button } from "./ui/Button";
@@ -12,15 +11,18 @@ import { Logo } from "./ui/Logo";
 
 const Header: React.FC = () => {
   const { t } = useTranslation();
-
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
     navigate("/demo");
   };
 
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
+  const handleToggleMenu = (event: React.MouseEvent) => {
+    event.stopPropagation(); // Prevent click from closing the menu
+    setIsOpen((prev) => !prev);
+  };
 
   const handleCloseMenu = (event: MouseEvent) => {
     if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
@@ -35,18 +37,16 @@ const Header: React.FC = () => {
     };
   }, []);
 
-  const handleToggleMenu = () => setIsOpen((prev) => !prev);
-
   return (
-    <header className="sticky top-0 z-50 overflow-x-clip py-2 backdrop-blur-2xl bg-background-80">
+    <header className="sticky top-0 z-50 py-2 backdrop-blur-md bg-background-80">
       <Container className="flex items-center justify-between">
         <Logo />
-        <nav className="hidden md:block">
+        <nav className="hidden md:flex">
           <ul className="flex items-center">
             <NavItem />
           </ul>
         </nav>
-        <div ref={menuRef}>
+        <div ref={menuRef} className="relative">
           <div className="hidden items-center gap-4 md:flex">
             <ToggleLocale />
             <Link onClick={handleClick} variant="primary" size="md">
@@ -62,7 +62,7 @@ const Header: React.FC = () => {
             <Icon icon={isOpen ? "close" : "menu"} />
           </Button>
           <div
-            className={`absolute right-2 top-20 flex w-52 flex-col gap-2 rounded-lg border bg-background p-2 transition-all duration-200 border-secondary-20 md:hidden ${isOpen ? "opacity-1 translate-x-0" : "translate-x-[150%] opacity-0"}`}
+            className={`absolute right-2 top-16 flex w-52 flex-col gap-2 rounded-lg border bg-background p-2 border-secondary-20 md:hidden ${isOpen ? "block" : "hidden"}`}
           >
             <nav>
               <ul>
