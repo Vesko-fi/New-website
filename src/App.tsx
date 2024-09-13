b7db7d4fea2bf2fe26f68b7f7e6201f2b86d7997
moscuet <mos.cuet@gmail.com>
2024-09-13 15:30:02 +0300

    Fixing :scrolling to top when navigate using navlink

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
 import i18n from "./locales/i18n";
 import { AppRouter } from "./router/AppRouter";
+import ScrollToTop from "@components/ScrollToTop";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <I18nextProvider i18n={i18n}>
         <Header />
         <main className="min-h-[90dvh]">
