3a67eb03abb08cef07ca18e041dd880ccfaaca7c
SantoshChapagai <you@example.com>
2024-09-13 10:59:49 +0300

    fix the text of demo link in join waitlist form

--- src/features/contact/ContactForm.tsx
+++ src/features/contact/ContactForm.tsx
@@ -59,7 +59,7 @@ const ContactForm: React.FC = () => {
       additional={
         <>
           <div>
-            <Label htmlFor="message">{t("contact.messagePlaceholder")}</Label>
+            <Label htmlFor="message">{t("contact.message")}</Label>
             <textarea
               id="message"
               className="block w-full resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
