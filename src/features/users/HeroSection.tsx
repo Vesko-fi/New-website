d0358026ab9648d77a95960e0ae8eb342fa10a32
sabin <sashre@utu.fi>
2024-10-01 09:36:26 +0300

    users page, fixed h1 to h2, added alt in images

--- src/features/users/HeroSection.tsx
+++ src/features/users/HeroSection.tsx
@@ -20,14 +20,15 @@ const HeroSection: React.FC = () => {
           <div className="h-full w-full bg-cover bg-center">
             <img
               src={UserHeroImg}
-              alt={`shopping`}
+              alt={`Users checking a product in-store while taking a picture for reference, showcasing the interactive shopping experience.`}
               className="h-full w-full object-cover"
+              loading="lazy"
             />
           </div>
           <div className="absolute inset-0 flex flex-col justify-center bg-black/30 px-8 py-12 text-white sm:px-8">
-            <h1 className="max-w-4xl text-3xl font-bold sm:text-balance md:text-5xl lg:text-6xl xl:text-7xl">
+            <h2 className="max-w-4xl text-3xl font-bold sm:text-balance md:text-5xl lg:text-6xl xl:text-7xl">
               {t("users.hero.heading")}
-            </h1>
+            </h2>
             <p className="xl:4xl mt-2 max-w-2xl text-xl sm:mt-4 md:text-2xl lg:mt-6 lg:text-3xl">
               {t("users.hero.text")}
             </p>
--- src/features/users/StoreSelling.tsx
+++ src/features/users/StoreSelling.tsx
@@ -13,9 +13,9 @@ const StoreSelling = () => {
     <Section>
       <Container className="flex flex-col items-center justify-between gap-4 md:flex-row md:gap-12">
         <div>
-          <h1 className="mb-4 text-2xl font-bold lg:text-3xl xl:text-4xl">
+          <h2 className="mb-4 text-2xl font-bold lg:text-3xl xl:text-4xl">
             {t("users.localShopping.heading")}{" "}
-          </h1>
+          </h2>
           <p className="text-sm xl:text-base">
             {t("users.localShopping.text")}
           </p>
@@ -23,6 +23,7 @@ const StoreSelling = () => {
         <img
           src={UserShoppingImg}
           className="rounded-xl sm:w-96 lg:w-[500px] xl:w-[650px]"
+          alt="Lady focused on a laptop screen displaying Vesko's product details page, highlighting her engagement in online shopping for unique products."
         />
       </Container>
     </Section>
