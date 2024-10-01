c75294ff30cb47491718788e0a8625922c8e940e
sabin <sashre@utu.fi>
2024-10-01 10:14:37 +0300

    seo update on home page

--- src/features/home/HeroSection.tsx
+++ src/features/home/HeroSection.tsx
@@ -17,7 +17,6 @@ const HeroSection: React.FC = () => {
       <SEO
         title={`${t("navbar.home.label")} | ${t("home.seo")}`}
         description={t("home.landingPage.text")}
-        keywords="Vesko home, vesko marketplace platform, omnichannel marketplace, build your brand online, maximize physical store sales, fulfill online orders from store, scalable store with inventory management, integrated payment and logistics, connect Finnish brands with consumers, Vesko omnichannel store, early access to Vesko marketplace"
       />
       <Section className="scroll-m-0 scroll-p-0">
         <Container className="flex flex-col items-center justify-between gap-8 lg:flex-row lg:gap-12 lg:py-24">
@@ -44,21 +43,21 @@ const HeroSection: React.FC = () => {
             {/* Background image */}
             <img
               src={heroImgBg}
-              alt="Vesko's landing page, hero section with organized display of various shoes in sleek, modern shoe cabinets with soft lighting, showcasing casual, formal, and athletic footwear."
+              alt="vesko's landing page, hero section with organized display of various shoes in sleek, modern shoe cabinets with soft lighting, showcasing casual, formal, and athletic footwear."
               className="w-full px-8"
               loading="lazy"
             />
             {/* Checkout image */}
             <img
               src={heroImgCheckout}
-              alt="Screen showing checkout section with product details, price, order to home, reserve product, and buy and pick up at store options."
+              alt="vesko's landing page image, screen showing checkout section with product details, price, order to home, reserve product, and buy and pick up at store options."
               className="absolute -top-16 right-0 w-24 sm:w-28 md:w-32 lg:w-36"
               loading="lazy"
             />
             {/* Product image */}
             <img
               src={heroImgProduct}
-              alt="Mobile screen displaying Vesko's product detail page with product image, thumbnails, name, brand, price, type, color, size, and other options."
+              alt="vesko's landing page image, Mobile screen displaying Vesko's product detail page with product image, thumbnails, name, brand, price, type, color, size, and other options."
               className="absolute -bottom-12 left-0 w-24 rounded-md sm:w-32 md:w-40 lg:w-48"
               loading="lazy"
             />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       <SEO
         title="Home"
         description="Welcome to Vesko"
-        keywords="vesko, services"
+        keywords="vesko home, vesko marketplace platform, omnichannel marketplace, build your brand online, maximize physical store sales, fulfill online orders from store, scalable store with inventory management, integrated payment and logistics, connect Finnish brands with consumers, Vesko omnichannel store, early access to Vesko marketplace"
       />
       <HeroSection />
       <StoreSection />
